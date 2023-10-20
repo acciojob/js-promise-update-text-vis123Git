ac3b35d9e7e7a813c6c999e0ce66b265acf278ea
Vishal Kumar Sain <102027742+vis123Git@users.noreply.github.com>
2023-10-20 19:33:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+const output = document.getElementById("output");
+
+
+const p1 = new Promise((resolve,reject)=>{
+	setTimeOut(()=>{
+			resolve("Hello, world!")
+	}, 1000)
+
+	reject("Failed!")
+})
+
+p1.then((data)=>{
+	output.innerText = data;
+}).catch((err)=>{
+	output.innerText = err.message;
+})
