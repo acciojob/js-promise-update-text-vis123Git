91b7db0c0cde3bfbbc9e450ee8d1ee4bc6e9ac5a
Vishal Kumar Sain <102027742+vis123Git@users.noreply.github.com>
2023-10-27 08:25:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const output = document.getElementById("output");
 
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("Hello, world!");
+    }, 1000);
+});
 
-const p1 = new Promise((resolve,reject)=>{
-	setTimeOut(()=>{
-			resolve("Hello, world!")
-	}, 1000)
+async function updateText() {
+    try {
+        const data = await p1;
+        output.innerText = data;
+    } catch (error) {
+        console.error(error);
+    }
+}
 
-	reject("Failed!")
-})
-
-p1.then((data)=>{
-	output.innerText = data;
-}).catch((err)=>{
-	output.innerText = err.message;
-})
+updateText();
